dc3258c102f4455ecc3c4dde7e875e62530a7f1a
intelliw <mchanmugam@gmail.com>
2019-09-09 16:12:11 +0800

    tested

--- sandbox/kafka-bq-testHarness.js
+++ sandbox/kafka-bq-testHarness.js
@@ -3,7 +3,9 @@
 
 const consts = require('../src/host/constants');
 const enums = require('../src/host/enums');
+
 const { Kafka } = require('kafkajs');
+const { BigQuery } = require('@google-cloud/bigquery');
 
 // const kafkaBrokerHost = '10.140.0.6';             // 10.140.0.6 / 35.201.177.2     192.168.1.106 
 const KAFKA_CONSUME_FROM_BEGINNING = true;
@@ -18,8 +20,8 @@ const kafka = new Kafka({
   clientId: consumerClientId,
 })
 
-const consumer = kafka.consumer({ 
-  groupId: consumerGroupId, 
+const consumer = kafka.consumer({
+  groupId: consumerGroupId,
   sessionTimeout: 30000,
   heartbeatInterval: 3000,
   rebalanceTimeout: 60000,
@@ -29,9 +31,10 @@ const consumer = kafka.consumer({
   minBytes: 1,
   maxBytes: 10485760,
   maxWaitTimeInMs: 5000,
-  retry: 10,  
+  retry: 10,
   readUncommitted: false
 })
+const bqClient = new BigQuery();                  // $env:GOOGLE_APPLICATION_CREDENTIALS="C:\_frg\_proj\190905-hse-api-consumer\credentials\sundaya-d75625d5dda7.json"
 
 const retrieve = async () => {
 
@@ -39,13 +42,25 @@ const retrieve = async () => {
   await consumer.subscribe({ topic: topicName, fromBeginning: KAFKA_CONSUME_FROM_BEGINNING })
   await consumer.run({
     eachMessage: async ({ topic, partition, message }) => {
-      console.log(`${topic} | P:${partition} | O:${message.offset} | Ts:${message.timestamp} | Key:${message.key} | Value: >>>> ${message.value} <<<<<`);
-    },
-
+      insertRows(topic, partition, message)
+      // console.log(`${topic} | P:${partition} | O:${message.offset} | Ts:${message.timestamp} | Key:${message.key} | Value: >>>> ${message.value} <<<<<`);
+    }
   })
 }
 
+async function insertRows(topic, partition, message) {
+  //let rows = [{"pms_id":"PMS-01-002","pack":{"id":"0248","dock":4,"volts":51.262,"amps":-0.625,"watts":-32.039,"temp":[35,33,34]},"cell":{"open":[1,6],"volts":[3.661,3.666,3.654,3.676,3.658,3.662,3.66,3.659,3.658,3.657,3.656,3.665,3.669,3.661],"vcl":3.654,"vch":3.676,"dvcl":[7,12,0,22,4,8,6,5,4,3,2,11,15,7]},"fet":{"open":[1,2],"temp":[34.1,32.2,33.5]},"sys":{"source":"S000"},"time_utc":"2019-02-09 08:00:17.0200","time_local":"2019-02-09 15:00:17.0200","time_processing":"2019-09-08 05:19:26.1940"},{"pms_id":"PMS-01-002","pack":{"id":"0248","dock":4,"volts":51.262,"amps":-0.625,"watts":-32.039,"temp":[35,33,34]},"cell":{"open":[1,6],"volts":[3.661,3.666,3.654,3.676,3.658,3.662,3.66,3.659,3.658,3.657,3.656,3.665,3.669,3.661],"vcl":3.654,"vch":3.676,"dvcl":[7,12,0,22,4,8,6,5,4,3,2,11,15,7]},"fet":{"open":[1,2],"temp":[34.1,32.2,33.5]},"sys":{"source":"S000"},"time_utc":"2019-02-09 08:00:17.0200","time_local":"2019-02-09 15:00:17.0200","time_processing":"2019-09-08 05:19:26.1940"}]
+  let rows = [JSON.parse(message.value)]
+
+  await bqClient
+    .dataset('monitoring')
+    .table('pms')
+    .insert(rows);
+  console.log(`Inserted ${rows.length} rows`);
+}
+
 retrieve().catch(e => console.error(`[${consumerClientId}] ${e.message}`, e))
+
 
 // exits for errors and terminal keyboard inputs  
 const errorTypes = ['unhandledRejection', 'uncaughtException']
@@ -67,7 +82,7 @@ errorTypes.map(type => {
 signalTraps.map(type => {
   process.once(type, async () => {
     try {
-      console.log(`signalTraps: process.once ${type}`)        
+      console.log(`signalTraps: process.once ${type}`)
       await consumer.disconnect()
     } finally {
       process.kill(process.pid, type)
