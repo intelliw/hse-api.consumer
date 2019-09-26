f1184559b17c67e30b1a7d99b0c06c60b1e493c9
intelliw <mchanmugam@gmail.com>
2019-09-26 10:27:05 +0800

    seperated app.js files per consumer

---
+++ inverter-app.js
@@ -0,0 +1,10 @@
+//@ts-check
+'use strict';
+/**
+ * monitor ALL topics and write to bq
+ */
+const Consumer = require('./src/consumers'); 
+
+// create consuemrs for each topic, they all share the same client id
+const inverter = new Consumer.BqInverter();      
+
---
+++ pms-app.js
@@ -0,0 +1,9 @@
+//@ts-check
+'use strict';
+/**
+ * monitor ALL topics and write to bq
+ */
+const Consumer = require('./src/consumers'); 
+
+// create consuemrs for each topic, they all share the same client id
+const pms = new Consumer.BqPms();
