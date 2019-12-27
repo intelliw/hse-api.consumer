357059f9428441a130dee436c7f0045dada0644b
intelliw <mchanmugam@gmail.com>
2019-12-27 14:03:03 +0800

    env configs

---
+++ src/consumers/PubSubConsumer.js
@@ -0,0 +1,107 @@
+//@ts-check
+"use strict";
+/**
+ * ./consumers/Consumer.js
+ *  base type for Kafka message consumers  
+ *  the Consumer supertype calls its subtype to transform messages (retrieveMessages() eachMessage:
+ *      the subtype contains a Consumer object and implements methods to transform and produce output 
+ *      - the Consumer supertype calls the subtype's transform method, which returns a transformed kafka message
+ *      - the Consumer supertype then performs generic transforms to the message, if any 
+ *      - it then calls the subtype's produce method with the transformed message
+ */
+const { Kafka } = require('kafkajs');
+const Consumer = require('./Consumer');
+
+const env = require('../environment/env');
+const utils = require('../environment/utils');
+const log = require('../logger').log;
+
+// exits for errors and terminal keyboard inputs  
+const errorTypes = ['unhandledRejection', 'uncaughtException']
+const signalTraps = ['SIGTERM', 'SIGINT', 'SIGUSR2']
+
+class KafkaConsumer extends Consumer{
+    /**9
+     * superclass - 
+     * 
+    instance attributes:  
+        this.kafkaConsumer
+        this.readTopic = readTopic;
+
+     constructor arguments 
+    * @param {*} groupId                                    //  enums.messageBroker.consumersGroups.monitoring
+    * @param {*} readTopic                                  //  the topic to read from env.active.messagebroker.topics.monitoring
+    */
+    constructor(groupId, readTopic) {
+
+        // store params
+        super(groupId, readTopic);
+        
+        // create the kafka consumer
+        const kafka = new Kafka({
+            brokers: env.active.kafka.brokers               //  e.g. [`${this.KAFKA_HOST}:9092`, `${this.KAFKA_HOST}:9094`]                                                       // https://kafka.js.org/docs/producing   
+        });
+        this.kafkaConsumer = kafka.consumer({ ...env.active.kafkajs.consumer, groupId: groupId });
+
+        // start the consumer    
+        this._initialiseTraps();
+        this._retrieveMessages().catch(e => log.error(`[${env.active.kafkajs.consumer.clientId}] Kafka consumer retrieve Error`, e))
+
+    }
+
+    // connect and listen for messages
+    async _retrieveMessages() {
+
+        const MESSAGE_PREFIX = 'KAFKA CONSUMER';
+
+        await this.kafkaConsumer.connect()
+            .then(this.kafkaConsumer.subscribe({ topic: this.readTopic, fromBeginning: env.active.kafkajs.consumer.consumeFromBeginning }))
+            .then(this.kafkaConsumer.run({
+                eachMessage: async ({ topic, partition, message }) => {
+
+                    // transform dataItems if required                                                                  // transformMonitoringDataset implemented by this super, it calls transformDataItem in subtype 
+                    let results = super.isMonitoringDataset() ? super.transformMonitoringDataset(message) : message;     // e.g. results: { itemCount: 9, messages: [. . .] }
+
+                    // write to bq and kafka topic
+                    this.produce(results);                                                                              // produce is implemented by subtype        
+
+                }
+            }))
+            .catch(e => log.error(`${MESSAGE_PREFIX} retrieveMessages() Error [${this.readTopic}]`, e));
+
+    }
+
+
+    // initialise error and signal traps
+    async _initialiseTraps() {
+
+        errorTypes.map(type => {
+            process.on(type, async e => {
+                try {
+                    console.log(`errorTypes: process.on ${type}`)
+                    log.error(`errorTypes: process.on ${type}`, e)
+                    await this.kafkaConsumer.disconnect()
+                    process.exit(0)
+                } catch (_) {
+                    process.exit(1)
+                }
+            })
+        })
+
+        // keyboard signal traps for terminal interrupts
+        signalTraps.map(type => {
+            process.once(type, async () => {
+                try {
+                    console.log(`signalTraps: process.once ${type}`)
+                    await this.kafkaConsumer.disconnect()
+                } finally {
+                    process.kill(process.pid, type)
+                }
+            })
+        })
+    }
+
+}
+
+
+module.exports = KafkaConsumer;
---
+++ src/producers/PubSubProducer.js
@@ -0,0 +1,71 @@
+//@ts-check
+"use strict";
+/**
+ * ./producers/Producer.js
+ *  base type for Kafka message producers  
+ *  subclass producer objects are responsible for transforming a consumed message and if requested, sending it to a new topic
+ */
+const { Kafka } = require('kafkajs');
+const Producer = require('./Producer');
+
+const enums = require('../environment/enums');
+const env = require('../environment/env');
+const log = require('../logger').log;
+
+const moment = require('moment');
+
+
+class KafkaProducer extends Producer {
+    /**
+     * superclass - 
+     * clients of subtypes must first call extractData(), then sendToTopic()
+     *  subtypes implement extractData by calling this superclass's addMessage() for each dataitem 
+     * 
+    instance attributes:  
+     producerObj": kafka.producer()
+     writeTopic:  env.active.messagebroker.topics.dataset                                         // this is the topic to which the subclassed producer writes, in sendTopic()  
+    */
+    constructor(writeTopic) {
+
+        super(writeTopic);
+
+        // create a kafka producer
+        const kafka = new Kafka({
+            brokers: env.active.kafka.brokers                                       //  e.g. [`${this.KAFKA_HOST}:9092`, `${this.KAFKA_HOST}:9094`]                                                       // https://kafka.js.org/docs/producing   
+        });
+
+        // setup instance variables specific to KafkaProducer 
+        this.producerObj = kafka.producer(env.active.kafkajs.producer);
+
+    }
+
+    /* creates messages for each item in the data array and sends the message array to the broker
+     * the transformResults object contains an array of kafka messages with modified data items
+     *      e.g. transformResults: { itemCount: 9, messages: [. . .] }
+     */
+    async sendToTopic(msgObj) {
+
+        // [start trace] -------------------------------
+        const sp = log.TRACE.createChildSpan({ name: `${log.enums.methods.kafkaSendToTopic}` });
+
+
+        // send the message to the topic
+        await this.producerObj.connect()
+            .then(() => this.producerObj.send({
+                topic: this.writeTopic,
+                messages: msgObj.messages,
+                acks: enums.messageBroker.ack.all,                                  // default is 'all'
+                timeout: env.active.kafkajs.send.timeout                            // milliseconds    
+            }))
+            .then(r => log.messaging(this.writeTopic, r[0].baseOffset, msgObj.messages, msgObj.itemCount, env.active.kafkajs.consumer.clientId))         // info = (topic, offset, msgqty, itemqty, sender) {
+            .then(this.producerObj.disconnect())
+            .catch(e => log.error(`${log.enums.methods.kafkaSendToTopic} Error [${this.writeTopic}]`, e));
+
+        // [end trace] -------------------------------
+        sp.endSpan();
+
+    }
+
+}
+
+module.exports = KafkaProducer;
