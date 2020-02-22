7d572be89823042e0b34dd0cf095923d5f22ebc6
intelliw <mchanmugam@gmail.com>
2020-02-22 14:52:31 +0800

    ok

--- src/storage/BqStorage.js
+++ src/storage/BqStorage.js
@@ -47,6 +47,7 @@ class BqStorage extends Storage {
         // [start trace] -------------------------------
         const sp = log.SPAN.createChildSpan({ name: `${log.enums.methods.bqInsertRows}` });
 
+
         // write to bq
         await rowArray.forEach(row => {                                     // rowArray = [{"pms_id":"PMS-01-002","pack_id":"0248","pack":{"volts":51.262,"amps":-0.625,"watts":-32.039,"vcl":3.654,"vch":3.676,"dock":4,"temp_top":35,"temp_mid":33,"temp_bottom":34},"cell":[{"volts":3.661,"dvcl":7,"open":false},{"volts":3.666,"dvcl":12,"open":false},{"volts":3.654,"dvcl":0,"open":false},{"volts":3.676,"dvcl":22,"open":false},{"volts":3.658,"dvcl":4,"open":false},{"volts":3.662,"dvcl":8,"open":false},{"volts":3.66,"dvcl":6,"open":false},{"volts":3.659,"dvcl":5,"open":false},{"volts":3.658,"dvcl":4,"open":false},{"volts":3.657,"dvcl":3,"open":false},{"volts":3.656,"dvcl":2,"open":false},{"volts":3.665,"dvcl":11,"open":true},{"volts":3.669,"dvcl":15,"open":false},{"volts":3.661,"dvcl":7,"open":false}],"fet_in":{"open":true,"temp":34.1},"fet_out":{"open":false,"temp":32.2},"status":{"bus_connect":true},"sys":{"source":"S000"},"time_event":"2019-02-09 08:00:17.0200","time_zone":"+07:00","time_processing":"2019-09-08 05:00:48.9830"}]
             this.storageObj
@@ -56,6 +57,7 @@ class BqStorage extends Storage {
                 .catch(e => log.error(`${log.enums.methods.bqInsertRows} Error  [${this.dataset}.${this.table}]`, e))
         });
 
+        
         // [end trace] ---------------------------------
         sp.endSpan();
 
